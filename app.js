3ee6cac6e38bc08f9b149d6b633f91437a4fb8e8
Juan Gallo <ji.gallo93@gmail.com>
2019-06-25 16:02:35 -0300

    cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const app = express();
+const cors = require("cors");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -23,14 +24,16 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
